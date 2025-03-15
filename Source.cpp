05fd7f2e7c09fe08d931c5057685a4c34e256b7f
standiaspines <stanwardpines@gmail.com>
2025-03-16 04:55:46 +0500

    can import messages as memos

--- Source.cpp
+++ Source.cpp
@@ -10,10 +10,16 @@ struct Data {
 	vector<string> msgs;
 };
 
-class Phone {
+struct Note {
+	string content;
+	string root;
+	string type;
+};
+
+class Phonebook {
 	public:
 		unordered_map <string, Data> database;
-		Phone(unordered_map<string, Data> &cnt) {
+		Phonebook(unordered_map<string, Data> &cnt) {
 			database = cnt;
 		}
 
@@ -35,6 +41,53 @@ class Phone {
  		}
 };
 
+class Notes {
+	public:
+		vector<Note> all_notes;
+
+		void importMSG(unordered_map<string, Data>& database) {
+			int i, chosen, to_import;
+			string ext_from;
+			vector<string> names;
+
+			i = 1;
+			for (const auto& contact : database) {
+				cout << "[" << i << "] " << contact.first << " :: (" << contact.second.num << ");\n";
+				names.push_back(contact.first);
+				i++;
+			}
+
+
+			cout << "Which chat are we importing a message from? [enter index]: ";
+			cin >> chosen;
+			ext_from = names[chosen - 1];
+
+
+			i = 1;
+			for (string msg : database[ext_from].msgs) {
+				cout << "[" << i << "] " << msg << "\n";
+				i++;
+			}
+
+			cout << "Which message are we importing? [enter index]: ";
+			cin >> to_import;
+			Note formedNote;
+			formedNote.content = database[ext_from].msgs[to_import];
+			formedNote.root = "pb";
+			formedNote.type = "txt";
+
+			all_notes.push_back(formedNote);
+		};
+		void showAll() {
+			int i = 1;
+			for (Note memo : all_notes) {
+				cout << "\nMemo #" << i << endl;
+				cout << memo.content << endl;
+				cout << "(" << memo.root << ")" << endl;
+			}
+		}
+};
+
 
 int main() {
 	unordered_map<string, Data> contacts;
@@ -46,12 +99,15 @@ int main() {
 	dame.msgs = dame_msgs;
 	contacts["Damian"] = dame;
 
-	Phone HuaweiP20Lite(contacts);
-	cout << HuaweiP20Lite.sendSMS("Damian", "Good Morning, nigga!") << endl;
-	for (string msg : HuaweiP20Lite.database["Damian"].msgs) {
+	Phonebook phone(contacts);
+	cout << phone.sendSMS("Damian", "Good Morning, nigga!") << endl;
+	for (string msg : phone.database["Damian"].msgs) {
 		cout << msg << endl;
 	}
 
+	Notes app;
+	app.importMSG(phone.database);
+	app.showAll();
 
 	return 0;
 }
